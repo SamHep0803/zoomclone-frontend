6ed63f5efffe6288f77374921a9c68c6f250592f
Samuel Hepworth <samhep0803@gmail.com>
2021-03-16 13:45:18 +0400

    stuff working

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,28 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import ApolloClient from "apollo-boost";
+import VueApollo from "vue-apollo";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueApollo);
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@/assets/css/main.css";
+
+const apolloClient = new ApolloClient({
+	// You should use an absolute URL here
+	uri: "http://localhost:4000/graphql",
+	credentials: "include",
+});
+
+const apolloProvider = new VueApollo({
+	defaultClient: apolloClient,
+});
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+	router,
+	apolloProvider,
+	render: (h) => h(App),
+}).$mount("#app");
